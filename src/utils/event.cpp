d79787369102e7d62c0b855127c2c6c4621a8c31
arnaavgoyal <58274830+arnaavgoyal@users.noreply.github.com>
2024-04-14 06:24:29 -0400

    Simplified event impl

--- src/utils/event.cpp
+++ src/utils/event.cpp
@@ -2,29 +2,3 @@
 
 #include "event.h"
 
-template <typename EventType>
-typename EventManager::Impl<EventType>::ListenerList EventManager::Impl<EventType>::listeners = ListenerList();
-
-// class MyCustomEvent1 { };
-// class MyCustomEvent2 { };
-
-// int main() {
-//     auto handle1 = EventManager::registerListener<MyCustomEvent1>(
-//         [](MyCustomEvent1){ std::cout << "Listener1 saw event occurred!\n"; }
-//     );
-//     auto handle2 = EventManager::registerListener<MyCustomEvent2>(
-//         [](MyCustomEvent2){ std::cout << "Listener2 saw event occurred!\n"; }
-//     );
-
-//     MyCustomEvent1 e1;
-//     MyCustomEvent2 e2;
-
-//     EventManager::trigger(e2);
-//     EventManager::trigger(e1);
-//     EventManager::trigger(e1);
-//     EventManager::deregisterListener(handle1);
-//     EventManager::trigger(e1);
-//     EventManager::trigger(e2);
-//     EventManager::deregisterListener(handle2);
-//     EventManager::trigger(e2);
-// }
--- src/utils/event.h
+++ src/utils/event.h
@@ -7,68 +7,58 @@
 #include <functional>
 #include <type_traits>
 
-class EventManager {
-private:
+namespace event {
 
-    template <typename EventType>
-    using Listener = std::function<void(EventType)>;
-    template <typename EventType>
-    struct ListenerHandle {
-        typename std::list<Listener<EventType>>::const_iterator iter;
-        bool valid;
-    };
+template <typename EventType>
+using Listener = std::function<void(EventType)>;
 
-    template <typename EventType>
-    struct Impl {
+template <typename EventType>
+struct ListenerHandle {
+    typename std::list<Listener<EventType>>::const_iterator iter;
+    bool valid;
+};
 
-        using ListenerList = std::list<Listener<EventType>>;
+template <typename EventType>
+struct Impl {
+    using ListenerList = std::list<Listener<EventType>>;
+    static ListenerList listeners;
 
-        static ListenerList listeners;
-
-        static void triggerImpl(EventType e) {
-            for (auto &&l : listeners) {
-                std::invoke(l, e);
-            }
+    static void triggerImpl(EventType e) {
+        for (auto &&l : listeners) {
+            std::invoke(l, e);
         }
-        static void trigger(EventType e) {
-            std::thread t(triggerImpl, e);
-            t.join();
-        }
-        static ListenerHandle<EventType> registerListener(Listener<EventType> &&l) {
-            listeners.push_back(l);
-            return ListenerHandle<EventType>{--listeners.end(), true};
-        }
-        static void deregisterListener(ListenerHandle<EventType> &lh) {
-            assert(lh.valid == true && "invalid listener ref");
-            listeners.erase(lh.iter);
-            lh.valid = false;
-        }
-    };
-
-public:
-
-    template <typename EventType>
-    static void trigger(EventType e) {
-        Impl<EventType>::trigger(std::forward<EventType>(e));
-    }
-
-    template <typename EventType>
-    static ListenerHandle<EventType> registerListener(Listener<EventType> &&l) {
-        return Impl<EventType>::registerListener(std::forward<Listener<EventType>>(l));
-    }
-    template <typename EventType>
-    static ListenerHandle<EventType> registerListener(EventType, Listener<EventType> &&l) {
-        return registerListenerFor<EventType>(std::forward<Listener<EventType>>(l));
-    }
-
-    template <typename EventType>
-    static void deregisterListener(ListenerHandle<EventType> &lh) {
-        Impl<EventType>::deregisterListener(lh);
-    }
-    template <typename EventType>
-    static void deregisterListener(EventType, ListenerHandle<EventType> &lh) {
-        Impl<EventType>::deregisterListener(lh);
     }
 };
+template <typename EventType>
+typename Impl<EventType>::ListenerList Impl<EventType>::listeners = ListenerList();
+
+template <typename EventType>
+void trigger(EventType &&e) {
+    std::thread t(Impl<EventType>::triggerImpl, std::forward<EventType>(e));
+    t.join();
+}
+
+template <typename EventType>
+ListenerHandle<EventType> registerListener(Listener<EventType> &&l) {
+    Impl<EventType>::listeners.push_back(std::forward<Listener<EventType>>(l));
+    return ListenerHandle<EventType>{--Impl<EventType>::listeners.end(), true};
+}
+template <typename EventType>
+ListenerHandle<EventType> registerListener(EventType, Listener<EventType> &&l) {
+    return registerListener<EventType>(std::forward<Listener<EventType>>(l));
+}
+
+template <typename EventType>
+void deregisterListener(ListenerHandle<EventType> &lh) {
+    assert(lh.valid == true && "invalid listener ref");
+    Impl<EventType>::listeners.erase(lh.iter);
+    lh.valid = false;
+}
+template <typename EventType>
+void deregisterListener(EventType, ListenerHandle<EventType> &lh) {
+    deregisterListener(lh);
+}
+
+} // namespace event
 
 #endif
